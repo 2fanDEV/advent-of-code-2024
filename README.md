1b28ab8d9c4b0feecb8c4b5f4193a3f4fedfab18
Tufan <tufan.oezen@protonmail.com>
2024-12-01 12:52:31 +0100

    Door 1 done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # advent-of-code-24
+These are solutions to the advent of code 2024 written in rust.
--- aoc1/src/main.rs
+++ aoc1/src/main.rs
@@ -1,3 +1,38 @@
+use std::{fs, i32};
+
 fn main() {
-    println!("Hello, world!");
+    let mut location_id_1: Vec<i32> = Vec::new();
+    let mut location_id_2: Vec<i32> = Vec::new();
+
+    let input = fs::read_to_string("../input/input").expect("Failed to read input file");
+    for line in input.lines() {
+        let split_line = line.split_once(" ").unwrap();
+        location_id_1.push(split_line.0.trim().parse().expect("Failed to parse number"));
+        location_id_2.push(split_line.1.trim().parse().expect("Failed to parse number"));
+    }
+    // part1(&mut location_id_1, &mut location_id_2);
+    part2(location_id_1, location_id_2);
+}
+
+fn part2(location_id_1: Vec<i32>, location_id_2: Vec<i32>) {
+    let mut similarity_score = 0;
+
+    for number in location_id_1 {
+        let count_in_loc2 = location_id_2.iter().filter(|&a| a.eq(&number)).count();
+        similarity_score = similarity_score + ((number as usize) * count_in_loc2);
+    }
+    println!("{}", similarity_score);
+}
+
+fn part1(location_id_1: &mut Vec<i32>, location_id_2: &mut Vec<i32>) {
+    location_id_1.sort();
+    location_id_2.sort();
+
+    let mut sum = 0;
+    for idx in 0..location_id_1.len() {
+        sum = sum
+            + (i32::max(location_id_1[idx], location_id_2[idx])
+                - (i32::min(location_id_1[idx], location_id_2[idx])));
+    }
+    println!("{}", sum);
 }
